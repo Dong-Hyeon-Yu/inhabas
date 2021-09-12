e314b7287c058383b9cab970a7ea4fb879c14f16
DongHyeon <ydh9516@gmail.com>
2021-09-12 16:38:27 +0900

    [invalid] 미디어 파일 위치 옮김

--- Back_end/IBAS/settings/base.py
+++ Back_end/IBAS/settings/base.py
@@ -188,7 +188,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # 미디어 파일을 관리할 루트 media 디렉터리
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 # 각 media file에 대한 URL prefix
 MEDIA_URL = '/media/'
 
