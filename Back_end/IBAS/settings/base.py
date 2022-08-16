3a991615abcf1cc9f455e48e864c0184ac2d6c99
유동현 <81698076+Dong-Hyeon-Yu@users.noreply.github.com>
2022-08-16 22:32:29 +0900

    설정파일에 smtp 계정 정보 설정

--- Back_end/IBAS/settings/base.py
+++ Back_end/IBAS/settings/base.py
@@ -203,8 +203,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'email-smtp.ap-northeast-2.amazonaws.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # 브라우져 종료시 세션 만료
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
